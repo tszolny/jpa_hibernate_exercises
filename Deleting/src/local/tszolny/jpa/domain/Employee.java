89928832f30a01d81a59c047c56fef7f56614c5e
tszolny <tszolny@gmail.com>
2020-02-18 20:15:44 +0100

    Usuwanie z bazy danych w hibernate

---
+++ Deleting/src/local/tszolny/jpa/domain/Employee.java
@@ -0,0 +1,43 @@
+package local.tszolny.jpa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String firstName;
+	private String lastName;
+	private double salary;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public long getId() {
+		return id;
+	}
+}
