3574242c48e40a31d765cc3a0e21ef2541637134
tszolny <tszolny@gmail.com>
2020-02-23 08:53:49 +0100

    One to one bidirectional zrobione

---
+++ One to One bidirectional/src/local/tszolny/jpa/domain/Owner.java
@@ -0,0 +1,45 @@
+package local.tszolny.jpa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Owner {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String firstName;
+	private String lastName;
+	@OneToOne
+	private Cat cat;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Cat getCat() {
+		return cat;
+	}
+
+	public void setCat(Cat cat) {
+		this.cat = cat;
+	}
+
+	public long getId() {
+		return id;
+	}
+}
